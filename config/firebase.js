d92039768a738a2a28788ef6653441edd85a5b9a
Aman Mittal <amandeepmittal@live.com>
2019-05-24 19:20:24 +0530

    add dotenv

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,23 @@
 import firebase from "firebase"
 
+import {
+	API_KEY,
+	AUTH_DOMAIN,
+	DATABASE_URL,
+	PROJECT_ID,
+	STORAGE_BUCKET,
+	MESSAGING_SENDER_ID,
+	APP_ID
+} from "react-native-dotenv"
+
 const config = {
-	apiKey: "AIzaSyBNVGTSNprnxeECDP8IP3HvbLSL0qK0FYk",
-	authDomain: "rnfirebase-e2de1.firebaseapp.com",
-	databaseURL: "https://rnfirebase-e2de1.firebaseio.com",
-	projectId: "rnfirebase-e2de1",
-	storageBucket: "rnfirebase-e2de1.appspot.com",
-	messagingSenderId: "670572703564",
-	appId: "1:670572703564:web:31f1c753185442b9"
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	databaseURL: DATABASE_URL,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: APP_ID
 }
 
 firebase.initializeApp(config)
