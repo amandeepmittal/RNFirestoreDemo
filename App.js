d67c5f0c3275c5daa4432a2a08ce1eda6904c5ae
Aman Mittal <amandeepmittal@live.com>
2019-05-25 02:13:58 +0530

    done

--- App.js
+++ App.js
@@ -1,49 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react"
+import { StyleSheet, SafeAreaView, Text, View, TextInput, Button } from "react-native"
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+	state = {
+		title: "",
+		author: ""
+	}
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+	render() {
+		const { title, author } = this.state
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+		return (
+			<SafeAreaView style={styles.container}>
+				<View style={styles.inputContainer}>
+					<TextInput
+						value={title}
+						placeholder='Title of the book'
+						style={styles.textInput}
+						onChangeText={value => this.setState({ title: value })}
+					/>
+					<TextInput
+						value={author}
+						placeholder='Name of the Author'
+						style={styles.textInput}
+						onChangeText={value => this.setState({ author: value })}
+					/>
+					<Button onPress={() => alert("Add the book")} title='Add the book' color='#841584' />
+				</View>
+			</SafeAreaView>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#F5FCFF"
+	},
+	inputContainer: {
+		margin: 30
+	},
+	textInput: {
+		height: 30,
+		textAlign: "center",
+		color: "#333333",
+		marginBottom: 10,
+		fontSize: 24,
+		borderWidth: 1,
+		borderBottomColor: "#111111"
+	}
+})
