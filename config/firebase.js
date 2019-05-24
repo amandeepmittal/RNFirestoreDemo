99956d0b4f84db62048abf7e60c7ddd3ca342f1b
Aman Mittal <amandeepmittal@live.com>
2019-05-24 18:49:05 +0530

    add firebase config

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase"
+
+const config = {
+	apiKey: "AIzaSyBNVGTSNprnxeECDP8IP3HvbLSL0qK0FYk",
+	authDomain: "rnfirebase-e2de1.firebaseapp.com",
+	databaseURL: "https://rnfirebase-e2de1.firebaseio.com",
+	projectId: "rnfirebase-e2de1",
+	storageBucket: "rnfirebase-e2de1.appspot.com",
+	messagingSenderId: "670572703564",
+	appId: "1:670572703564:web:31f1c753185442b9"
+}
+
+firebase.initializeApp(config)
+
+export default firebase
